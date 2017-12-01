cedb6e8ddd16e7751577a168427fd17ca0616ec2
Jean-Eudes <jean-eudes.patois@utbm.fr>
2017-12-01 13:40:45 +0100

    Project can be launched with no argument (default value 3)

--- QIteration/src/fr/utbm/qiteration/QIteration.java
+++ QIteration/src/fr/utbm/qiteration/QIteration.java
@@ -3,24 +3,30 @@ package fr.utbm.qiteration;
 public class QIteration {
 
 	public static void main(String[] args) {
-		Tableau myTableau = new Tableau(Integer.valueOf(args[0]));
+		Tableau myTableau = null;
+		if(args.length != 0) { // Pas de vérification si args[0] est bien un nombre...
+			myTableau = new Tableau(Integer.valueOf(args[0]));
+		} else {
+			myTableau = new Tableau(3); // Tableau par défaut
+		}
+			
 		System.out.println(myTableau.toString());
 		Tableau tempTableau = null;
 		myTableau.initCases();
 		
 		int reward = 1;
 		float gamma = 0.5f;
-		int i = 1;
+		int iteration = 1;
 		
 		do {
 			tempTableau = new Tableau(myTableau);
 			tempTableau.initCases();
 			myTableau.updateTableau(reward, gamma, tempTableau);
-			System.out.println("Tableau " + i);
+			System.out.println("Iteration #" + iteration);
 			System.out.println(myTableau.toString());
 			//System.out.println("tempTableau");
 			//System.out.println(tempTableau.toString());
-			++i;
+			++iteration;
 		} while(!myTableau.compareTableaux(tempTableau));
 	}
 
